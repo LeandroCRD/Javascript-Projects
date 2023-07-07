32ef3017fb57d876ec313051f5f733514a9b5b2c
Leandro Dionísio <81056784+LeandroCRD@users.noreply.github.com>
2023-07-07 11:17:31 +0100

    Add files via upload

---
+++ myapp.js
@@ -0,0 +1,19 @@
+//Javascript Essentials | 37.Final Project | Udemy - Lawrence Turton
+
+function set()
+{
+    let elements = document.getElementsByName('cssProperty');
+
+    for(let index = 0; index <elements.length; index++)
+    {
+        let cssProperty = elements[index].getAttribute('id');
+
+        let cssValue = elements[index].value;
+
+        let div = document.getElementById('modify');
+
+        div.style[cssProperty]=cssValue;
+    }
+}
+
+document.getElementById('set').addEventListener('click', set);
